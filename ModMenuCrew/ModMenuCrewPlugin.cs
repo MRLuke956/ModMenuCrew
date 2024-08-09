ddb21cb0dfb764f09519d35d1a3dde71d7155082
Luke Dennyel <65508738+MRLuke956@users.noreply.github.com>
2024-08-08 21:07:18 -0300

    SOURCE AFTER YEARS

---
+++ ModMenuCrew/ModMenuCrewPlugin.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Attributes;
+using Reactor.Utilities.Attributes;
+using Reactor.Utilities.ImGui;
+using UnityEngine;
+using Reactor;
+using Hazel;
+using AmongUs.GameOptions;
+
+namespace ModMenuCrew;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class ModMenuCrewPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public DebuggerComponent Component { get; private set; } = null!;
+
+    public override void Load()
+    {
+        Component = this.AddComponent<DebuggerComponent>();
+        Harmony.PatchAll();
+      
+    }
+   
+
+
+
+    [RegisterInIl2Cpp]
+    public class DebuggerComponent : MonoBehaviour
+    {
+        [HideFromIl2Cpp]
+        public bool DisableGameEnd { get; set; }
+        public bool Impostor { get; set; }
+        public bool IsNoclipping { get; set; }
+        bool previousNoclipState;
+        public uint NetId;
+       
+
+        internal static class RandomHelper
+        {
+            private static Il2CppSystem.Random random = new Il2CppSystem.Random();
+
+            public static byte GetRandomByte()
+            {
+                return (byte)random.Next(0, 15);
+            }
+        }
+        byte randomByte = RandomHelper.GetRandomByte();
+        [HideFromIl2Cpp]
+        public DragWindow TestWindow { get; }
+
+        public DebuggerComponent(IntPtr ptr) : base(ptr)
+        {
+            string GenerateRandomId(int length)
+            {
+                string id = Guid.NewGuid().ToString();
+
+                return id.Substring(0, length);
+            }
+
+            // Chamar quando criar o menu
+            string randomId = GenerateRandomId(7);
+
+            TestWindow = new DragWindow(new Rect(15, 15, 100, 100), $"BETA MOD MENU BY MRLukex {randomId}", () =>
+            {
+
+
+                if (!ShipStatus.Instance)
+                {
+
+                    GUILayout.Label("\r Join A Game", GUILayout.ExpandWidth(true));
+                    string banTime = StatsManager.Instance.BanMinutes.ToString("F0");
+                    string iAmBan = StatsManager.Instance.AmBanned.ToString();
+                    if (GUILayout.Button($"ban ({banTime} min left,\b am i banned? ({iAmBan})"))
+                    {
+                        StatsManager.Instance.BanPoints += 100;
+
+                    }
+                    if (GUILayout.Button("removeban"))
+                    {
+
+                        StatsManager.Instance.BanPoints = 0;
+                    }
+
+                }
+
+
+                if (ShipStatus.Instance && AmongUsClient.Instance.AmClient)
+                {
+                    MessageWriter messageWriter2 = AmongUsClient.Instance.StartRpc(this.NetId, 6, SendOption.Reliable);
+                    messageWriter2.Write(name);
+                    messageWriter2.EndMessage();
+
+
+                    // NO WORKING GUILayout.Label("Name: " + DataManager.Player.Customization.Name);
+                    //   DisableGameEnd = GUILayout.Toggle(DisableGameEnd, "Disable game end (Host Only)", GUILayout.ExpandWidth(true));
+
+
+                    if (GUILayout.Button("Force game end (Host Only)"))
+                    {
+
+                        ShipStatus.Instance.enabled = false;
+                        GameManager.Instance.RpcEndGame(GameOverReason.ImpostorBySabotage, false);
+                        return;
+
+                    }
+
+                    if (GUILayout.Button("Call a meeting"))
+                    {
+                        PlayerControl.LocalPlayer.CmdReportDeadBody(null);
+
+                    }
+                    if (GUILayout.Button("Color Changer"))
+                    {
+                            byte randomByte = RandomHelper.GetRandomByte();
+                            PlayerControl.LocalPlayer.CmdCheckColor(randomByte);
+                    }
+
+                        if (GUILayout.Button("Scanner"))
+                        {
+                            byte b = (byte)(PlayerControl.LocalPlayer.scannerCount + 1);
+                            PlayerControl.LocalPlayer.scannerCount = b;
+                            byte b2 = b;
+                            PlayerControl.LocalPlayer.SetScanner(true, b2);
+                            if (AmongUsClient.Instance.AmClient)
+                            {
+                                MessageWriter messageWriter = AmongUsClient.Instance.StartRpc(this.NetId, (byte)RpcCalls.SetScanner, SendOption.Reliable);
+                                messageWriter.StartMessage((byte)RpcCalls.SetScanner);
+                            
+                                messageWriter.Write(true);
+                                messageWriter.Write(b2);
+                                messageWriter.EndMessage();
+                            }
+                        }
+
+                        if (PlayerControl.LocalPlayer)
+                        {
+
+                            var noclipButton = GUILayout.Toggle(IsNoclipping, "Noclip");
+
+                            if (noclipButton != IsNoclipping)
+                            {
+
+                                if (noclipButton)
+                                {
+
+                                    //Muda estado
+                                    PlayerControl.LocalPlayer.Collider.enabled = false;
+                                    IsNoclipping = true;
+
+
+                                    //Compara com estado anterior
+                                    if (previousNoclipState != IsNoclipping)
+                                    {
+
+                                        AddNotification("OFF", "ON");
+
+                                    }
+
+                                    previousNoclipState = IsNoclipping;
+
+                                }
+                                else
+                                {
+
+                                    //Muda estado 
+                                    PlayerControl.LocalPlayer.Collider.enabled = true;
+                                    IsNoclipping = false;
+
+                                    //Compara com estado anterior
+                                    if (previousNoclipState != IsNoclipping)
+                                    {
+
+                                        AddNotification("ON", "OFF");
+
+                                    }
+
+                                    previousNoclipState = IsNoclipping;
+
+                                }
+
+                            }
+
+                        }
+
+
+                        void AddNotification(string previous, string current)
+                        {
+
+                            string message = $"Noclip -> {current} ";
+
+                            DestroyableSingleton<HudManager>.Instance.Notifier
+                              .AddItem($"{message}");
+
+                        }
+
+                        if (!PlayerControl.LocalPlayer.Data.Role.IsImpostor && ShipStatus.Instance.MapPrefab)
+                        {
+                            if (GUILayout.Button("BeImpostor"))
+                            {
+                                PlayerControl.LocalPlayer.SetRole(RoleTypes.Shapeshifter);
+                                PlayerControl.LocalPlayer.Data.Role.CanUseKillButton = false;
+
+                            }
+                        }
+
+                        if (GUILayout.Button("Teleport Closest Player"))
+                        {
+                            PlayerControl closestPlayer = GetClosestPlayer();
+
+                            if (closestPlayer)
+                            {
+
+                                PlayerControl.LocalPlayer.transform.position = closestPlayer.transform.position;
+
+                            }
+
+                        }
+
+                        PlayerControl GetClosestPlayer()
+                        {
+                            PlayerControl closest = null;
+                            float closestDistance = Mathf.Infinity;
+
+                            foreach (PlayerControl player in PlayerControl.AllPlayerControls)
+                            {
+                                if (player == PlayerControl.LocalPlayer) continue;
+                                if (!player.Data.IsDead)
+                                {
+
+
+                                    float distance = (player.transform.position - PlayerControl.LocalPlayer.transform.position).sqrMagnitude;
+
+                                    if (distance < closestDistance)
+                                    {
+                                        closest = player;
+                                        closestDistance = distance;
+                                    }
+                                }
+
+                            }
+
+                            return closest;
+                        }
+                    }
+
+                    var players = GameData.Instance.AllPlayers;
+                    foreach (var player in players)
+                    {
+                        if (!player.Role.IsImpostor)
+                        {
+                            continue;
+                        }
+                        player.PlayerName.Color(Color.blue);
+                        // Informações básicas
+                        string status = player.Role.IsImpostor
+                         ? $"<color=red>Sim</color>"
+                         : "Não";
+
+                        GUILayout.Label($"Nome: {player.PlayerName} - Impostor: {status}");
+
+                    }
+
+                }
+            )
+            {
+                Enabled = false,
+            };
+
+        }
+        public void Draw()
+        {
+            GUILayout.BeginVertical("TestWindow");
+
+            GUILayout.HorizontalScrollbar(15f,15f,15f,15);
+            GUILayout.BeginHorizontal();
+
+            // elementos horizontais
+           
+
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+            // etc
+
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                TestWindow.Enabled = !TestWindow.Enabled;
+            }
+        }
+
+        private void OnGUI()
+        {
+            TestWindow.OnGUI();
+        }
+    }
+
+
+}
---
+++ ModMenuCrew/skinunlocked.cs
@@ -0,0 +1,97 @@
+﻿using HarmonyLib;
+using Hazel;
+using InnerNet;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+namespace ModMenuCrew
+{
+    internal class SkinUnlockPatch
+    {
+        [HarmonyPatch(typeof(HatManager), nameof(HatManager.Initialize))]
+        internal class UnlockCosmetics
+        {
+            public static void Postfix(HatManager __instance)
+            {
+                foreach (var bundle in __instance.allBundles)
+                { bundle.Free = true; }
+
+                foreach (var featuredBundle in __instance.allFeaturedBundles)
+                { featuredBundle.Free = true; }
+
+                foreach (var featuredCube in __instance.allFeaturedCubes)
+                { featuredCube.Free = true; }
+
+                foreach (var featuredItem in __instance.allFeaturedItems)
+                { featuredItem.Free = true; }
+
+                foreach (var hat in __instance.allHats)
+                { hat.Free = true; }
+
+                foreach (var nameplate in __instance.allNamePlates)
+                { nameplate.Free = true; }
+
+                foreach (var pet in __instance.allPets)
+                { pet.Free = true; }
+
+                foreach (var skin in __instance.allSkins)
+                { skin.Free = true; }
+
+                foreach (var starBundle in __instance.allStarBundles)
+                { starBundle.price = 0; }
+
+                foreach (var visor in __instance.allVisors)
+                { visor.Free = true; }
+            }
+        }
+
+    }
+    [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.CanBan))]
+    internal class InnerNetClient_CanBan_Patch
+    {
+
+        [HarmonyPrefix]
+        public static bool Prefix(InnerNetClient __instance)
+        {
+            // Retorna true sempre, permitindo o ban para qualquer jogador
+            return true;
+
+        }
+
+
+    }
+    [HarmonyPatch(typeof(ChatController), nameof(ChatController.Update))]
+    internal class test
+    {
+
+        public static void Postfix(ChatController __instance)
+        {
+            __instance.banButton.enabled = true;
+            __instance.banButton.gameObject.SetActive(true);
+            __instance.banButton.BanButton.gameObject.SetActive(true);
+        }
+    }
+
+    [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.Update))]
+    internal class RainbowFriendCodePatch
+    {
+
+        public static void Postfix(EOSManager __instance)
+        {
+
+            string friendCodeText = $"<color=#f52626>Amy#Admin</color>";
+
+            __instance.friendCode = friendCodeText;
+            __instance.FriendCode = friendCodeText;
+            __instance.friendsListKey = friendCodeText.ToLower();
+
+
+            // PlayerControl.LocalPlayer.name = "ADM\n\nAMY";
+            // PlayerControl.LocalPlayer.CmdCheckName("ADM\n\nAMY");
+        }
+
+    }
+
+   
+}
